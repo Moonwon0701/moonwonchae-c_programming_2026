e08c81155965d9a3bedb7ec5d7979b79be3999ae
Moonwon Chae <145437553+Moonwon0701@users.noreply.github.com>
2026-03-06 04:50:10 +0000

    added second code

---
+++ lec0/2nd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // hello world 출력
+    printf("Hello, World!\n");
+    /*  
+        여러줄의 주석을 넣을 수 있다는 거임~
+        보기 좋다는 거임!   
+    */  
+
+    return 0;
+}
