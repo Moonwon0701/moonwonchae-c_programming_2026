463fed2b3478c90ca774390fd4dd6ddb0748ec35
Moonwon Chae <145437553+Moonwon0701@users.noreply.github.com>
2026-03-18 04:22:18 +0000

    lec about 3 term operator

--- lec2/01.c
+++ lec2/01.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
 int main() {
-    
-    
+    int a = 3; 
+    int b = 0;
+    printf("a: %d\n", !a);
+    printf("b: %d\n", !b);
+
+    printf("%d\n", a||b);
+    printf("%d\n", a&&b);
+
+    printf("3 == 2 : %c\n", 3 == 2 ? 'T' : 'F');
+    printf("3 > 2 : %c\n", 3 > 2 ? 'T' : 'F');
+    printf("3 < 2 : %c\n", 3 < 2 ? 'T' : 'F');
+
+    printf("%d\n", 0.1 + 0.9 == 1.0);
     return 0;
 }
